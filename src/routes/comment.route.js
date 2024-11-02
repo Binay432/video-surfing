5ff18d8a8dfcf649d69f1af44f58932cee327bfa
Binay432 <sahbinay432@gmail.com>
2024-11-02 20:25:07 +0545

    added delete comment endpoints

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,12 +1,18 @@
 import { Router } from "express"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { addComment, getVideoComments } from "../controller/comment.controller.js";
+import { 
+    addComment, 
+    deleteComment, 
+    getVideoComments, 
+    updateComment 
+} from "../controller/comment.controller.js";
 
 const router = Router(); 
 
 //apply to all routes 
 router.use(verifyJwt);
 router.route('/add-comment').post(addComment)
-router.route('/get-comments/v/:videoId').get(getVideoComments)
-
+router.route('/get-comments/:videoId').get(getVideoComments)
+router.route('/update-comment/:commentId').patch(updateComment)
+router.route('delete-comment/:commentId').delete(deleteComment)
 export default router
