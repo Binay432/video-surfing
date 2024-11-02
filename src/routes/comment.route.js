5575ce10178e5eaef025d539d220991a3df2afab
Binay432 <sahbinay432@gmail.com>
2024-11-02 19:51:07 +0545

    added get video comments endpoints

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { addComment } from "../controller/comment.controller.js";
+import { addComment, getVideoComments } from "../controller/comment.controller.js";
 
 const router = Router(); 
 
 //apply to all routes 
 router.use(verifyJwt);
 router.route('/add-comment').post(addComment)
+router.route('/get-comments/v/:videoId').get(getVideoComments)
 
 export default router
